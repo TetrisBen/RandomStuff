46047db4284655b4d31c83d77c616b65b2d5bd25
TetrisBen <ben.rasper@gmail.com>
2020-07-23 15:30:46 -0500

    Add files via upload

---
+++ 69420.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int genRandomNumber(){
+    std::srand((unsigned) time(0));
+    int randomNumber = 0;
+    int max = 99999;
+    int min = 10000;
+    int range = max - min;
+    int count;
+    while(randomNumber != 69420) {
+        randomNumber = (std::rand() % range + min);
+        count++;
+        std::cout << randomNumber << std::endl;
+    }
+
+    std::cout << "Iterations: " << count << std::endl;
+    return randomNumber;    
+}    
+    
+int main() {
+    genRandomNumber();
+}
