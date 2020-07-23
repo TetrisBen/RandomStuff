81950d3f0c57c81cd2dfdcc1f86ac9f5795cedef
basedtho <60894565+basedtho@users.noreply.github.com>
2020-07-23 14:18:59 -0700

    Clang-format - good!
    Make it a habit

--- 69420.cpp
+++ 69420.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 
-int genRandomNumber(){
-    std::srand((unsigned) time(0));
-    int randomNumber{0};
-    int max{99999};
-    int min{10000};
-    int range{max - min};
-    int count{0};
-    while(randomNumber != 69420) {
-        randomNumber = (std::rand() % range + min);
-        count++;
-        std::cout << randomNumber << std::endl;
-    }
+int genRandomNumber() {
+  std::srand((unsigned)time(0));
+  int randomNumber{0};
+  int max{99999};
+  int min{10000};
+  int range{max - min};
+  int count{0};
+  while (randomNumber != 69420) {
+    randomNumber = (std::rand() % range + min);
+    count++;
+    std::cout << randomNumber << std::endl;
+  }
 
-    std::cout << "Iterations: " << count << std::endl;
-    return randomNumber;    
-}    
-    
-int main() {
-    genRandomNumber();
+  std::cout << "Iterations: " << count << std::endl;
+  return randomNumber;
 }
+
+int main() { genRandomNumber(); }
